9ce5459218bdf424b0d8a535866fb0f14b25d0d7
CarlosV951 <57165427+CarlosV951@users.noreply.github.com>
2024-12-08 17:12:25 -0600

    [ADD]APP_temporal

--- Python-Modelo/tokens.py
+++ Python-Modelo/tokens.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from tensorflow.keras.preprocessing.text import Tokenizer
 import pickle
+import json
 
 # Cargar el archivo CSV limpio
 data = pd.read_csv('data_clean/data_clean.csv', sep=',', header=None, names=['input', 'target'])
@@ -17,7 +18,7 @@ data['input'] = data['input'].astype(str)
 data['target'] = data['target'].astype(str)
 
 # Agregar tokens de inicio (<start>) y fin (<end>) a las frases objetivo
-data['target'] = data['target'].apply(lambda x: '<start> ' + x + ' <end>')
+data['target'] = data['target'].apply(lambda x: 'sos ' + x + ' eos')
 
 # Verificar que los tokens se agregaron correctamente
 print("\nDatos después de agregar tokens <start> y <end>:")
@@ -41,8 +42,8 @@ with open('tokenizer_target.pkl', 'wb') as f:
 print("\nTokenizers reconstruidos y guardados como 'tokenizer_input.pkl' y 'tokenizer_target.pkl'")
 
 # Verificar los índices de los tokens especiales
-print(f"Índice del token <start>: {tokenizer_target.word_index.get('<start>')}")
-print(f"Índice del token <end>: {tokenizer_target.word_index.get('<end>')}")
+print(f"Índice del token <sos>: {tokenizer_target.word_index.get('<sos>')}")
+print(f"Índice del token <eos>: {tokenizer_target.word_index.get('<eos>')}")
 
 # Ejemplo de secuencia tokenizada
 example_input = data['input'].iloc[0]
@@ -56,3 +57,20 @@ print(f"Frase de entrada original: {example_input}")
 print(f"Tokens de entrada: {input_seq}")
 print(f"Frase objetivo original: {example_target}")
 print(f"Tokens de salida: {target_seq}")
+
+# ------------- Hacer la conversion a JSON ---------------------
+# Cargar los tokenizers desde los archivos pickle
+with open('tokenizer_input.pkl', 'rb') as f:
+    tokenizer_input = pickle.load(f)
+
+with open('tokenizer_target.pkl', 'rb') as f:
+    tokenizer_target = pickle.load(f)
+
+# Guardar los tokenizers como JSON
+with open('tokenizer_input.json', 'w') as f:
+    json.dump(tokenizer_input.word_index, f)
+
+with open('tokenizer_target.json', 'w') as f:
+    json.dump(tokenizer_target.word_index, f)
+
+print("Tokenizers convertidos a JSON correctamente.")
