e6f425246f4fc93cb17fb1a2fb4f9cc71313b5b4
Tukito <201503756@ingenieria.usac.edu.gt>
2024-12-20 00:55:50 -0600

    setup gh-pages

--- F2/Front/vite.config.js
+++ F2/Front/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://netuko13.github.io/IA1_Proyecto_15', // Link page
 })
